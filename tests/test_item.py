8d2d1378b87d3ea0958768aa5774b895ba01bb8a
anastasia_lykova <anastasia.7@mail.ru>
2023-06-07 04:05:47 +0500

    testing Item class 100% coverage

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_smartphone():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_item_init(item_smartphone):
+    assert item_smartphone.name == "Смартфон"
+    assert item_smartphone.price == 10000
+    assert item_smartphone.quantity == 20
+
+
+def test_calculate_total_price(item_smartphone):
+    assert item_smartphone.calculate_total_price() == 200000
+
+
+def test_apply_discount(item_smartphone):
+    Item.pay_rate = 0.8
+    item_smartphone.apply_discount()
+    assert item_smartphone.price == 8000.0
